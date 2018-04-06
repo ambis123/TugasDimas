c379434e4df136e3bd596ec07551d212f17901ab
ambis123 <38097225+ambis123@users.noreply.github.com>
2018-04-07 00:56:51 +0700

    Buat

--- src/tugasdimas/TugasDimas1.java
+++ src/tugasdimas/TugasDimas1.java
@@ -11,4 +11,20 @@ package tugasdimas;
  */
 public class TugasDimas1 {
     
+    String Nama;
+    String NPM;
+    String Kelas;
+    String Class;
+    String Atribut;
+    String Behavior;
+    String Objek;
+    
+    void cetakInfo(){
+            System.out.println("Nama : "+Nama);
+            System.out.println("NPM : "+NPM);
+            System.out.println("Kelas : "+Kelas);
+            System.out.println("Class : "+Class);
+            System.out.println("Objek : "+Objek);
+            System.out.println("Atribut : "+Atribut);
+    }
 }
