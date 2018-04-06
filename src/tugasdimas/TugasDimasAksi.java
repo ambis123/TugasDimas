2f2df82f781d784cbdd0210210fc9f364cc5a238
ambis123 <38097225+ambis123@users.noreply.github.com>
2018-04-07 01:22:53 +0700

    Isi

--- src/tugasdimas/TugasDimasAksi.java
+++ src/tugasdimas/TugasDimasAksi.java
@@ -12,6 +12,20 @@ package tugasdimas;
 public class TugasDimasAksi {
     public static void main(String[] args) {
         TugasDimas1 Dimas = new TugasDimas1();
-    
+        
+  Dimas.Nama="Dimas Risalni M Noor";
+  Dimas.NPM="16630318";
+  Dimas.Kelas="4A Nonreg";
+  Dimas.Class="Pantai";
+  Dimas.Objek="Pantai Bali, Pantai Lombok,Pantai Aceh";
+  Dimas.Atribut="Pantai Bali= Warna Pasir : Putih Kecoklatan,Ombak Laut : Besar"
+   + "Wisatawan : Lokal dan Luar, Kebersihan Pantai : Sedang,"
+   + "Warna Air Laut: Biru Tua, Pantai Lombok= Warna Pasir: Putih Susu,"
+          + "Ombak Laut: Sedang, Wisatawan : Asing, Kebersihan Pantai: Bersih"
+          + "Warna Air Laut: Biru Muda, Pantai Aceh= Warna Pasir: Coklat"
+          + "Ombak Laut: Kecil, Wisatawan : Lokal, Kebersihan Pantai: kotor"
+          + "Warna Air Laut: Biru Jernih ";
+  Dimas.Behavior=" Tempat Berlibur";
+  
 }
 }
